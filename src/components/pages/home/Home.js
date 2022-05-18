347fb2cbab3bf234228196b527aa8335ba56895b
unknown <racagnisasha@gmail.com>
2022-05-18 15:26:29 -0300

    borre nuestras marcas viejo

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import Entregas from './entregas/Entregas';
-import Brands from './Brands';
 import './Home.css';
 
 
 function Home(){
     return(
         <>
-        <div className='container-title'>
-            <p className='title'>HOME</p>
-        </div>    
-        <div className='container-title'>
-                <p className='title' style={{fontSize: '20px'}}>Nuestras Marcas</p>
-                <Brands/>  
-        </div>  
         <Entregas/>
         </>
     );
