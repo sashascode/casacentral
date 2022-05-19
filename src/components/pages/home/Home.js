cc1fd5e3a24246c25be51371f84ad5d6d7d9f313
Bruno Redzio <brunoredzio@hotmail.com>
2022-05-19 00:12:09 -0300

    feature/encabezado home
    -Se realizo encabezado de pagina principal
    -Se agregó boton a link de modelos
    -Se agregó imagen de fondo

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Entregas from './entregas/Entregas';
+import Encabezado from './encabezado/Encabezado';
+
 import './Home.css';
 import Testimonios from './testimonios/Testimonios';
 
@@ -7,6 +9,7 @@ import Testimonios from './testimonios/Testimonios';
 function Home(){
     return(
         <>
+        <Encabezado/>
         <Entregas/>
         <Testimonios/>
         </>
