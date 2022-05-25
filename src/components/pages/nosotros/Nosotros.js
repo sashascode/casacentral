55690e8015a2ef4364c3b7dd89ca27f10c213e0a
Bruno Redzio <brunoredzio@hotmail.com>
2022-05-25 18:14:59 -0300

    Se agregaron valores
    Se agregó valores: mision, vision, calidad

--- src/components/pages/nosotros/Nosotros.js
+++ src/components/pages/nosotros/Nosotros.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Encabezado from '../../shared/encabezado/Encabezado';
 import fondo from '../../../images/nosotrosfinal.jpg';
 import './Nosotros.css';
+import Valores from './valores/Valores';
 
 
 function Nosotros(){
     return(
         <>
             <Encabezado title="Somos un equipo de valientes" fondo={fondo}/>
-            <div style={{height:"550px"}}></div>
+            <Valores/>
         </>
     );
 }
