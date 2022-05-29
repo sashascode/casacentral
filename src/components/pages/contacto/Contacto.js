3a2938555e4789911d933fb67994cc40e0d00474
Bruno Redzio <brunoredzio@hotmail.com>
2022-05-29 17:31:38 -0300

    Datos de contacto
    Se agregaron datos de contacto al lado del formulario

--- src/components/pages/contacto/Contacto.js
+++ src/components/pages/contacto/Contacto.js
@@ -3,13 +3,17 @@ import Encabezado from '../../shared/encabezado/Encabezado';
 import fondo from '../../../images/contactofinal.jpg';
 import './Contacto.css';
 import Formulario from './formulario/Formulario';
+import DatosDeContacto from './datosDeContacto/DatosDeContacto';
 
 
 function Contacto(){
     return(
         <>
             <Encabezado title="¡Háblanos y te asesoraremos!" fondo={fondo}/>
-            <Formulario/>
+            <div className="container d-flex justify-content-center align-items-center h-100" style={{padding:'5rem 1rem'}}>
+                <DatosDeContacto/>
+                <Formulario/>
+            </div>
         </>
     );
 }
--- src/components/pages/home/testimonios/cards/Card.js
+++ src/components/pages/home/testimonios/cards/Card.js
@@ -4,13 +4,13 @@ import "./Card.css";
 
 function Card({ imageSource, title, text }) {
   return (
-    <div className="card text-center bg-dark animate__animated animate__fadeInUp">
+    <div className="card text-center tarjeta animate__animated animate__fadeInUp">
       <div className="overflow">
         <img src={imageSource} alt="cliente" className="card-img-top" />
       </div>
-      <div className="card-body text-light">
+      <div className="card-body text-light tarjetas">
         <h4 className="card-title">{title}</h4>
-        <p className="card-text text-secondary">
+        <p className="card-text text-light">
           {text
             ? text
             : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam deserunt fuga accusantium excepturi quia, voluptates obcaecati nam in voluptas perferendis velit harum dignissimos quasi ex? Tempore repellat quo doloribus magnam."}
