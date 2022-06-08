63c13498905979abd0c16d898a9796b553a13d69
Bruno Redzio <brunoredzio@hotmail.com>
2022-06-08 01:11:17 -0300

    Estilos filtros modelos
    Se ajustaron estilos
    Se ajusto que al moverse entre menus vuelva al top de la pantalla

--- src/components/pages/modelos/ModelosNav.js
+++ src/components/pages/modelos/ModelosNav.js
@@ -6,14 +6,38 @@ const ModelosNav = () => {
     <nav class="navbar-fixed-top" style={{backgroundColor: '#445c9c'}} role="navigation">
         <div className="container-fluid" style={{paddingTop: '100px'}}>
             <ul class="nav navbar-nav d-flex flex-row justify-content-around">
-                <NavLink style={{textDecoration: 'none'}} to={'/marcas/fiat'} className={({isActive}) => isActive ? "nav-marca-active" : "nav-marca"}><li className="p-3 display-6 nav-marca">Fiat</li></NavLink>
-                <NavLink style={{textDecoration: 'none'}} to={'/marcas/volkswagen'} className={({isActive}) => isActive ? "nav-marca-active" : "nav-marca"}><li className="p-3 display-6 nav-marca">VW</li></NavLink>
-                <NavLink style={{textDecoration: 'none'}} to={'/marcas/renault'} className={({isActive}) => isActive ? "nav-marca-active" : "nav-marca"}><li className="p-3 display-6 nav-marca">Renault</li></NavLink>
-                <NavLink style={{textDecoration: 'none'}} to={'/marcas/jeep'} className={({isActive}) => isActive ? "nav-marca-active" : "nav-marca"}><li className="p-3 display-6 nav-marca">Jeep</li></NavLink>
-                <NavLink style={{textDecoration: 'none'}} to={'/marcas/citroen'} className={({isActive}) => isActive ? "nav-marca-active" : "nav-marca"}><li className="p-3 display-6 nav-marca">Citroen</li></NavLink>
-                <NavLink style={{textDecoration: 'none'}} to={'/marcas/peugeot'} className={({isActive}) => isActive ? "nav-marca-active" : "nav-marca"}><li className="p-3 display-6 nav-marca">Peugeot</li></NavLink>
-                <NavLink style={{textDecoration: 'none'}} to={'/marcas/ford'} className={({isActive}) => isActive ? "nav-marca-active" : "nav-marca"}><li className="p-3 display-6 nav-marca">Ford</li></NavLink>
-                <NavLink style={{textDecoration: 'none'}} to={'/marcas/toyota'} className={({isActive}) => isActive ? "nav-marca-active" : "nav-marca"}><li className="p-3 display-6 nav-marca">Toyota</li></NavLink>
+            <li className='p-3 display-6 nav-marca'>
+                <NavLink to={'/marcas/fiat'} className={({isActive}) => isActive ? "nav-marca-active" : "nav-marca"}>Fiat</NavLink>
+            </li>
+
+            <li className='p-3 display-6 nav-marca'>
+                <NavLink style={{textDecoration: 'none'}} to={'/marcas/volkswagen'} className={({isActive}) => isActive ? "nav-marca-active" : "nav-marca"}>VW</NavLink>
+            </li>
+
+            <li className='p-3 display-6 nav-marca'>
+                <NavLink style={{textDecoration: 'none'}} to={'/marcas/renault'} className={({isActive}) => isActive ? "nav-marca-active" : "nav-marca"}>Renault</NavLink>
+            </li>
+
+            <li className='p-3 display-6 nav-marca'>
+                <NavLink style={{textDecoration: 'none'}} to={'/marcas/jeep'} className={({isActive}) => isActive ? "nav-marca-active" : "nav-marca"}>Jeep</NavLink>
+            </li>
+            
+            <li className='p-3 display-6 nav-marca'>
+                <NavLink style={{textDecoration: 'none'}} to={'/marcas/citroen'} className={({isActive}) => isActive ? "nav-marca-active" : "nav-marca"}>Citroen</NavLink>
+            </li>
+
+            <li className='p-3 display-6 nav-marca'>
+                <NavLink style={{textDecoration: 'none'}} to={'/marcas/peugeot'} className={({isActive}) => isActive ? "nav-marca-active" : "nav-marca"}>Peugeot</NavLink>
+            </li>
+
+            <li className='p-3 display-6 nav-marca'>
+                <NavLink style={{textDecoration: 'none'}} to={'/marcas/ford'} className={({isActive}) => isActive ? "nav-marca-active" : "nav-marca"}>Ford</NavLink>
+            </li>
+
+            <li className='p-3 display-6 nav-marca'>
+                <NavLink style={{textDecoration: 'none'}} to={'/marcas/toyota'} className={({isActive}) => isActive ? "nav-marca-active" : "nav-marca"}>Toyota</NavLink>
+            </li>
+
             </ul>
         </div>
     </nav>
--- src/components/shared/navbar/Menu.js
+++ src/components/shared/navbar/Menu.js
@@ -3,12 +3,11 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-d
 import Home from "../../pages/home/Home";
 import Nosotros from "../../pages/nosotros/Nosotros";
 import Contacto from "../../pages/contacto/Contacto";
-
-
 import Navbar from "./Navbar";
 import Modelos from "../../pages/modelos/Modelos";
 import ModeloContainer from '../../pages/modelos/modelo/ModeloContainer';
 import ModelosNav from "../../pages/modelos/ModelosNav";
+import ScrolltoTop from "./ScrolltoTop";
 
 
 
@@ -18,6 +17,7 @@ function Menu(){
         <div>
            <Router>
                 <Navbar/>
+                <ScrolltoTop/>
                 <Routes>
                     <Route exact path="/nosotros"  element={<Nosotros/>}/>
                     <Route exact path="/modelos" element={<><ModelosNav/> <Modelos/></>}/>
