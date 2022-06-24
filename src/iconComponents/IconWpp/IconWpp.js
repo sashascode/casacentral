a601e4956ca44239969f5d3a6df231dac6543951
unknown <racagnisasha@gmail.com>
2022-06-24 18:40:49 -0300

    cambio de numero de telefono

--- src/iconComponents/IconWpp/IconWpp.js
+++ src/iconComponents/IconWpp/IconWpp.js
@@ -4,7 +4,7 @@ import './IconWpp.css'
 
 const IconWpp = () => {
   return (
-    <a href='https://api.whatsapp.com/send?phone=5491167927272&text=Hola!%20Quisiera%20%20hacer%20una%20consulta,%20vengo%20de%20la%20página%20de%20Casa%20Central.' className='btn-wsp' target={'_blank'}>
+    <a href='https://api.whatsapp.com/send?phone=5491153887019&text=Hola!%20Quisiera%20%20hacer%20una%20consulta,%20vengo%20de%20la%20página%20de%20Casa%20Central.' className='btn-wsp' target={'_blank'}>
       <i className='icon-whatsapp'><ImWhatsapp/></i>
     </a>
   )
